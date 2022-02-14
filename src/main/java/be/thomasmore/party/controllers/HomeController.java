05f7e5d607d590abee267bdf955873ddcb04aa96
SandervanRiet <r0903044@student.thomasore.be>
2022-02-14 16:42:53 +0100

    home en about pagina wijzigen

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -1,17 +1,26 @@
 package be.thomasmore.party.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping({"/", "/home"})
-    public String home() {
+    public String home(Model model) {
+        int myCalculatedValue = 34 * 62;
+        model.addAttribute("myCalculatedValue", myCalculatedValue);
         return "home";
     }
 
     @GetMapping("/about")
-    public String about() {
+    public String about(Model model) {
+        String myName= "Sander";
+        model.addAttribute("myName", myName);
+        String myCity = "Schelle";
+        model.addAttribute("myCity", myCity);
+        String myStreet= "goedendagtraat";
+        model.addAttribute("myStreet", myStreet);
         return "about";
     }
 
